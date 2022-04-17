885c5b80a8d45edaf5e953a81524a02a5f3eb5e4
CamposCaio <camposcaioh@gmail.com>
2022-04-17 00:16:05 -0300

    fixed type error

--- src/components/_sections/Technologies/index.tsx
+++ src/components/_sections/Technologies/index.tsx
@@ -37,7 +37,7 @@ export function SectionTechnologies() {
   return (
     <Container id="technologies">
       <Fade bottom>
-        <Content color={activeTheme.palette.text.divider}>
+        <Content color={activeTheme.palette.text.secondary}>
           <h2>{t.technologiesTitle}</h2>
           <h6>{t.technologiesSubtitle}</h6>
           <Paper elevation={4}>
